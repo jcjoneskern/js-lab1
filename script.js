9e9c072f290bfd439b8ab045cc9e922f4bfa7718
J-C <jcjoneskern@gmail.com>
2017-10-13 22:13:31 -0400

    pt3

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-// JavaScript Lab Part Two
-// Task: Expand on the game by adding functions that allow the user to start the game and get a number to use as damage.
+// JavaScript Lab Part Three
+// Task: Convert the variables relating to Grant and the user’s character into objects. Add a heal method to the character’s object. The user can only heal two times throughout the duration of the game.
 //
 // What does the application do?
-// The application now has a startGame function, which will prompt the user if they would like to play. Call the startCombat function after the user enters a character’s name.
-// There is a startCombat function. When executed, this will run the loop you created in Part One.
-// There is a getDamage function. This will return a number between 1 and 5 that will be used to decide how much damage the user and Grant will deal each round.
+// The character is now an object.
+// Grant is now an object.
+// The properties attack and heal are methods for the character object, and attack is a method for the Grant object.
+// Make sure the game tracks the character’s heal usage.
+
+var player = {
+  name: "",
+  health: 40,
+  wins: 0,
+  healCount: 0,
+  attack: function() {
+    return Math.floor((Math.random() * 3) + 1);
+  },
+  heal: function() {
+    this.healCount++;
+    return Math.floor((Math.random() * 10) + 1);
+  }
+}
+
+var grant = {
+  name: "Grant",
+  health: 10,
+  attack: function() {
+    return Math.floor((Math.random() * 5) + 1);
+  }
+}
 
 function startGame() {
   var gameStart = prompt("Would you like to play a game?");
@@ -16,51 +39,52 @@ function startGame() {
 }
 
 function startCombat(name) {
-  var userHealth = 40;
-  var grantHealth = 10;
-  var winCount = 0;
+  player.name = name;
 
-  while (userHealth > 0) {
-    var userChoice = prompt("Attack or Quit?");
+  while (player.health > 0) {
+    var userChoice = prompt("Attack, Heal, or Quit?");
 
     if (userChoice.toLowerCase() === "attack") {
-      userHealth -= getDamage();
-      grantHealth -= getDamage();
+      player.health -= grant.attack();
+      grant.health -= player.attack();
 
-      console.log(name + " has " + userHealth + "hp");
-      console.log("Grant has " + grantHealth + "hp");
+      console.log(player.name + " has " + player.health + "hp");
+      console.log("Grant has " + grant.health + "hp");
 
-      if (userHealth <= 0) {
+      if (player.health <= 0) {
         console.log("You lose! Grant wins!");
-      } else if (grantHealth <= 0 && winCount === 2) {
-        winCount++;
-        console.log("Grant is defeated! " + name + " wins!");
+      } else if (grant.health <= 0 && player.wins === 2) {
+        player.wins++;
+        console.log("Grant is defeated! " + player.name + " wins!");
         break;
-      } else if (grantHealth <= 0) {
-        winCount++;
-        console.log("Grant is defeated! You must still win " + (3 - winCount) + " more time(s).");
-        grantHealth = 10;
+      } else if (grant.health <= 0) {
+        player.wins++;
+        console.log("Grant is defeated! You must still win " + (3 - player.wins) + " more time(s).");
+        grant.health = 10;
       }
     } else if (userChoice.toLowerCase() === "quit") {
       console.log("Got away safely!");
       break;
+    } else if (userChoice.toLowerCase() === "heal") {
+      if (player.healCount < 2) {
+        player.heal();
+        console.log(player.name + " heals and has " + player.health + "hp. " + player.name + " has used " + player.healCount + " out of 2 heal casts.");
+        player.health -= grant.attack();
+        console.log("Grant attacks! " + player.name + " now has " + player.health + "hp");
+      } else {
+        console.log(player.name + " can no longer heal! Choose another option.");
+      }
     } else {
-      alert("You must enter \"attack\" or \"quit.\"");
+      alert("You must enter \"attack,\" \"heal,\" or \"quit.\"");
     }
   }
 }
 
-function getDamage() {
-  return Math.floor((Math.random() * 5) + 1);
-}
-
-startGame();
-
 // Build Specifications:
-// Each iteration of the loop will include a prompt that will ask the user if they would like to “attack” or “quit”.
-// If the user decides to attack, adjust the character’s health points and Grant’s health points based on the getDamage function.
-// If the user decides to quit, figure out a way to exit out of the loop and function.
-
-// potential pitfalls:
-// -perpetually being prompted w/o seeing console logs (this resolved itself?? not sure what happened)
-// -else condition if user does not type attack or quit (...........)
+// The character must have the following properties
+// name, health, wins, healCount, attack, and heal
+// Grant must have the following properties
+// name, health, attack
+// The user’s attack method should return a number between 1 and 3 (this has changed from the original numbers).
+// The user’s heal method should add a number between 1 and 10 to the character’s health and change the healCount number.
+// Grant’s attack method should return a number between 1 and 5.
